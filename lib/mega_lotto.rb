c11fae4ad12ad1bb47811874992085c132d58008
houman <houman.kargaran@gmail.com>
2015-06-29 22:56:52 +1000

    initial commit

---
+++ lib/mega_lotto.rb
@@ -0,0 +1,11 @@
+require "mega_lotto/version"
+require "mega_lotto/drawing"
+
+begin
+	require "pry"
+rescue LoadError	
+end
+
+module MegaLotto
+  # Your code goes here...
+end
---
+++ spec/mega_lotto/drawing_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+
+module MegaLotto
+	describe Drawing do
+		describe "#draw" do
+
+			let(:drawing) { Drawing.new.draw  }
+
+			it "returns an array" do
+				expect(drawing).to be_a(Array)
+			end
+
+			it "returns array with 5 elements" do
+				expect(drawing.size).to eq(5)
+			end
+
+			it "each element is an Integer" do
+				drawing.each do |item|
+					expect(item).to be_a(Integer)
+				end
+			end
+
+			it "each element is < 60" do
+				drawing.each do |item|
+					expect(item).to be < 60
+				end
+			end
+
+
+		end
+	end
+end
